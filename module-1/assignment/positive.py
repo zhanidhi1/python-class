3e6109f4f4a50dfa473aed5b4d18ec42ae5c6c9a
zhanidhi1 <njha4792@gmail.com>
2024-01-26 07:45:57 +0545

    sec commit

---
+++ module-1/assignment/positive.py
@@ -0,0 +1,11 @@
+def positive_negative(number):
+    if number==0:
+        print("its a zero")
+    elif number>0:
+        print("its a positive number")
+    else:
+        print("its a negative number")
+
+
+find=int (input("enter a number"))
+positive_negative(find)

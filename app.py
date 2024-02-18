7e48ee90a7635ecb161bff08bd1c4cb05514c8c2
zhanidhi1 <njha4792@gmail.com>
2024-02-18 15:00:26 +0545

    streamlit

---
+++ app.py
@@ -0,0 +1,4 @@
+import streamlit as st
+
+st.write("Hello Dear friends")
+st.image('https://images.pexels.com/photos/417074/pexels-photo-417074.jpeg?cs=srgb&dl=pexels-james-wheeler-417074.jpg&fm=jpg')

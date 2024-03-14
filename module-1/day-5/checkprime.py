d2a7ae1c9c08e99fe21da5ca650de592c335b060
zhanidhi1 <njha4792@gmail.com>
2024-03-14 18:40:07 +0545

    flask-app

--- module-1/day-5/checkprime.py
+++ module-1/day-5/checkprime.py
@@ -24,3 +24,32 @@ if is_prime(number):
 else:
     print(f"{number} is not a prime number.")
     print_multiplication_table(number)
+
+
+
+def is_prime(number):
+    if number <= 1:
+        return False
+    for i in range(2, int(number ** 0.5) + 1):
+        if number % i == 0:
+            return False
+    return True
+
+def calculate_f_of_x(x):
+    return x**2 + 5*x + 2
+
+def print_multiplication_table(number):
+
+    print(f"Multiplication table of {number}:")
+    for i in range(1, 11):
+        print(f"{number} x {i} = {number * i}")
+
+number = int(input("Enter a number: "))
+
+if is_prime(number):
+    print(f"{number} is a prime number.")
+    result = calculate_f_of_x(number)
+    print(f"f({number}) = {result}")
+else:
+    print(f"{number} is not a prime number.")
+    print_multiplication_table(number)    
